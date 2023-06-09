abef20c19e457e2453c2440bf4676f7857d4db0c
Felipe Escalona <escalonaf12@gmail.com>
2023-06-09 12:36:25 -0400

    First commit

---
+++ src/main/java/com/felipe/automercadoFelipe/AutomercadoFelipeApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.automercadoFelipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomercadoFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomercadoFelipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/automercadoFelipe/AutomercadoFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.automercadoFelipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomercadoFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
